1d3b6c6693b97f1b1978e64fe026763b271a5e1e
spanda <spanda@tavisca.in>
2019-08-23 05:12:51 +0530

    Sorta working

--- Script.js
+++ Script.js
@@ -1,24 +1,127 @@
-function search(){
-    var x=document.getElementById("text").value;
+// Delete Function
+function removeElement(ele)
+{
+  console.log(ele.innerHTML);
+  var index = tasks.indexOf(ele.innerHTML);
+  if (index > -1) {
+    tasks.splice(index, 1);
+  }
 
+  document.getElementById("tasks").removeChild(ele);
 
 }
 
-function add(){
-    var inputValue=document.getElementById("text").value;
+//Search Function
+function searchTasks(){
+  var x=document.getElementById("text").value;
+  var i=0;
+  for(i=0;i<tasks.length;i++)
+  {
+    if(tasks[i]==x)
+    {
+      alert("To Do: "+x);
+      break;
+    }
+  }
+  if(i==tasks.length)
+    alert("Not Found");
+  document.getElementById("text").value = "";
+  document.getElementById('datalist').innerHTML = '';
 
-
-    if (inputValue === '') {
-        alert("Field is empty!");
-      } else {
-
-        var li = document.createElement("li");
-        var node = document.createTextNode(inputValue);
-        li.appendChild(node);
-        document.getElementById("taks").appendChild(li);
-
-      }
-
-      document.getElementById("text").value = "";
 }
 
+
+// Add Task Function
+function add(){ 
+  
+  var inputValue=document.getElementById("text").value;
+
+
+  if (inputValue === '') {
+      alert("Field is empty!");
+    } else {
+      
+      tasks.push(inputValue);
+      
+
+      var li = document.createElement("li");
+      var node = document.createTextNode(inputValue+" ");
+      
+      //var td1= document.createElement("td");
+      //var td2= document.createElement("td");
+      
+      li.appendChild(node);
+      //td2.appendChild(node);
+      
+      //tr.appendChild(td1);
+      li.className="checked";
+
+      var button1= document.createElement("button");
+      button1.type="button";
+      button1.innerHTML="Delete";
+      button1.onclick="removeElement(this)";
+      li.append(button1);
+      document.getElementById("tasks").appendChild(li);
+     
+
+    }
+
+    document.getElementById("text").value = "";
+    document.getElementById('datalist').innerHTML = '';
+
+}
+
+
+function hideSuggestions()
+{
+  document.getElementById('datalist').innerHTML = '';
+}
+
+//Autosuggestion fuction
+function suggest(data) {
+
+  document.getElementById('datalist').innerHTML = '';
+   
+  for (var i = 0; i<tasks.length; i++) {
+   if(((tasks[i].toLowerCase()).indexOf(data.toLowerCase()))>-1)
+   {
+     
+     var node = document.createElement("option");
+     var t = document.createTextNode(tasks[i]);
+     node.appendChild(t);
+     node.className="sugeestions";
+     document.getElementById("datalist").appendChild(node);
+     
+    }
+  }
+}
+
+
+
+
+function showContent(){
+  
+  for(i=0;i<tasks.length;i++)
+  {
+   	var li = document.createElement("li");
+    var node = document.createTextNode(tasks[i]+" ");
+
+      
+    li.appendChild(node);
+      
+    li.className="checked";
+
+    var button1= document.createElement("button");
+    button1.type="button";
+    button1.innerHTML="Delete";
+    button1.onclick="removeElement(this)";
+    li.append(button1);
+    document.getElementById("tasks").appendChild(li);
+    
+  }
+}
+
+
+
+
+var tasks=["Breakfast","Meet Shomu","Buy Grocery","Finish Assignments","Laundry"];
